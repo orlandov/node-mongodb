ce9bcf590946536d1050f6ba2850fb2dfa3244b8
Orlando Vazquez <ovazquez@gmail.com>
2009-12-04 15:57:08 -0800

    add support for top level string, number and booleans

--- bson.cc
+++ bson.cc
@@ -7,26 +7,58 @@ extern "C" {
 
 using namespace v8;
 
+const char* ToCString(const v8::String::Utf8Value& value) {
+  return *value ? *value : "<string conversion failed>";
+}
+
 Handle<Value>
 encode(const Arguments &args) {
     // TODO assert args.length > 0
-
+    // TODO assert args.type == Object
+    
     HandleScope scope;
+    Local<Object> obj = args[0]->ToObject();
+    Local<Array> properties = obj->GetPropertyNames();
 
+    bson        bson;
     bson_buffer bb;
-    bson obj;
-    bson_oid_t oid;
-    char *buffer;
+    bson_oid_t  oid;
 
-   // bson_oid_gen(&oid);
-    {
-        bson_buffer_init(&bb);
-     //   bson_append_oid(&bb, "_id", &oid);
-        bson_append_string(&bb, "hello", "world");
-        bson_from_buffer(&obj, &bb);
+    bson_buffer_init(&bb);
+
+    for (int i = 0; i < properties->Length(); i++) {
+        // get the property name
+        Local<Value> prop_name = properties->Get(Integer::New(i));
+        Local<Value> prop_val = obj->Get(prop_name->ToString());
+
+        // convert the name to a c string
+        v8::String::Utf8Value n(prop_name);
+        const char *name = ToCString(n);
+
+        if (prop_val->IsString()) {
+            String::Utf8Value v(prop_val);
+            const char *value = ToCString(v);
+            bson_append_string(&bb, name, value);
+        }
+        else if (prop_val->IsInt32()) {
+            double value = prop_val->NumberValue();
+            bson_append_double(&bb, name, value);
+        }
+        else if (prop_val->IsNumber()) {
+            double value = prop_val->NumberValue();
+            bson_append_double(&bb, name, value);
+        }
+        else if (prop_val->IsBoolean()) {
+            char value = prop_val->IsTrue() ? 1 : 0;
+            bson_append_bool(&bb, name, value);
+        }
+        else {
+            printf("nothing!!\n");
+        }
     }
-
-    return String::New(obj.data, bson_size(&obj));
+    
+    bson_from_buffer(&bson, &bb);
+    return String::New(bson.data, bson_size(&bson));
 }
 
 extern "C" void
@@ -36,3 +68,4 @@ init (Handle<Object> target) {
         String::New("encode"),
         FunctionTemplate::New(encode)->GetFunction());
 }
+
--- test_bson.js
+++ test_bson.js
@@ -3,6 +3,100 @@ process.mixin(GLOBAL, require('mjsunit'));
 sys = require("sys");
 bson = require("./bson");
 
-assertEquals(
-    bson.encode({"hello": "world"}),
-    "\x16\x00\x00\x00\x02hello\x00\x06\x00\x00\x00world\x00\x00");
+function xxdCompare(expected, actual) {
+    var proc = process.createChildProcess("xxd", ["-i"]);
+    var buffer = '';
+
+    proc.addListener("output", function (data) {
+        buffer += data;
+    });
+
+    proc.addListener("exit", function (code) {
+        // start the second process
+
+        var proc2 = process.createChildProcess("xxd", ["-i"]);
+        var buffer2 = '';
+
+        proc2.addListener("output", function (data) {
+            buffer2 += data;
+        });
+
+        proc2.addListener("exit", function (code) {
+            assertEquals(buffer, buffer2);    
+        });
+
+        proc2.write(actual);
+        proc2.close();
+    });
+
+    proc.write(expected);
+    proc.close();
+}
+
+function xxdPrint(str) {
+    var proc = process.createChildProcess("xxd");
+    var buffer = '';
+
+    proc.addListener("output", function (data) {
+        sys.puts("got a line");
+        buffer += data;
+    });
+    proc.addListener("exit", function (code) {
+        sys.puts("buffer was ");
+        sys.puts(buffer);        
+    });
+    proc.write(str);
+    proc.close();
+}
+
+// strings
+{
+    xxdCompare(
+        bson.encode({ "hello": "world" }),
+        "\x16\x00\x00\x00\x02hello\x00\x06\x00\x00\x00world\x00\x00");
+
+    xxdCompare(
+        bson.encode({ "javascript": "rocks", "mongodb": "rocks" }),
+        "\x2e\x00\x00\x00"          + // header
+        "\x02javascript\x00"        + // key
+        "\x06\x00\x00\x00rocks\x00" + // value
+        "\x02mongodb\x00"           + // key
+        "\x06\x00\x00\x00rocks\x00" + // value
+        "\x00"
+    );
+}
+
+// numbers
+{
+
+    xxdCompare(
+        bson.encode({ "hello": 1 }),
+        "\x14\x00\x00\x00"                 + // header
+        "\x01hello\x00"                    + // key
+        "\x00\x00\x00\x00\x00\x00\xfd\x3f" + // value
+        "\x00");
+
+    xxdCompare(
+        bson.encode({ "hello": 3.141 }),
+        "\x14\x00\x00\x00"                 + // header
+        "\x01hello\x00"                    + // key
+        "\x54\x5b\xfd\x20\x09\x40" + // value
+        "\x00");
+}
+
+// booleans
+{
+    xxdCompare(
+        bson.encode({ "hello": true }),
+        "\x0d\x00\x00\x00" + // header
+        "\x08hello\x00"    + // key
+        "\x01"             + // value
+        "\x00");
+
+    xxdCompare(
+        bson.encode({ "hello": false }),
+        "\x0d\x00\x00\x00" + // header
+        "\x08hello\x00"    + // key
+        "\x00"             + // value
+        "\x00");
+}
