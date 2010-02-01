afa36574e045bde42cd85e201c9adfbddbff31a1
Orlando Vazquez <ovazquez@gmail.com>
2010-02-01 00:34:08 -0800

    refactor unit tests to use assert.js instead of mjsunit.js

--- tests/test_mongo.js
+++ tests/test_mongo.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-process.mixin(GLOBAL, require('mjsunit'));
+process.mixin(GLOBAL, require('assert'));
 
 var jjj = JSON.stringify;
 
@@ -17,7 +17,7 @@ var mongo = new mongodb.MongoDB();
 var cb = 0;
 
 mongo.addListener("close", function () {
-    assertEquals(cb, 11);
+    equal(cb, 11);
     sys.puts("Tests done!");
 });
 
@@ -26,18 +26,18 @@ mongo.addListener("connection", function () {
     mongo.getCollections().addCallback(function (collections) {
         cb++;
         collections.sort();
-        assertEquals(["system.indexes", "widgets"], collections);
+        deepEqual(collections, ["system.indexes", "widgets"]);
     });
     var widgets = mongo.getCollection('widgets');
 
-    assertThrows("widgets.remove([1,2])");
-    assertThrows("widgets.remove(1)");
+    throws("widgets.remove([1,2])");
+    throws("widgets.remove(1)");
 
     widgets.remove();
 
     widgets.count().addCallback(function(count) {
         cb++;
-        assertEquals(0, count);
+        equal(count, 0);
 
         widgets.insert({ _id: new mongodb.ObjectID(oid_hex), "dude": "lebowski" });
 
@@ -48,37 +48,37 @@ mongo.addListener("connection", function () {
 
         widgets.find({ baz: 420 }).addCallback(function (result) {
             cb++;
-            assertEquals(result[0].bucket, [1, 2]);
+            deepEqual(result[0].bucket, [1, 2]);
         });
 
         widgets.find_one({ _id: new mongodb.ObjectID(oid_hex) }).addCallback(function (result) {
             cb++;
-            assertEquals(result._id.toString(), oid_hex);
-            assertEquals(result.dude, "lebowski");
+            equal(result._id.toString(), oid_hex);
+            equal(result.dude, "lebowski");
         });
 
         widgets.count().addCallback(function (count) {
             cb++;
-            assertEquals(5, count);
+            equal(5, count);
         });
 
         widgets.count({ shazbot: { "$lte": 1 } }).addCallback(function (count) {
             cb++;
-            assertEquals(2, count);
+            equal(2, count);
         });
 
         widgets.find().addCallback(function (results) {
             cb++;
-            assertEquals(results.length, 5);
+            equal(5, results.length);
         });
 
         widgets.find({ shazbot: { "$gt": 0 } }).addCallback(function (results) {
             cb++;
-            assertEquals(results.length, 2);
+            equal(results.length, 2);
             results.forEach(function (r) {
                 // check that we had a validish oid
-                assertTrue(r['_id'].toString().length == 24);
-                assertEquals(r['baz'], undefined);
+                ok(r['_id'].toString().length == 24);
+                equal(r['baz'], undefined);
             });
         });
 
@@ -87,19 +87,19 @@ mongo.addListener("connection", function () {
             var shazbots = [];
             results.forEach(function (r) {
                 shazbots.push(r.shazbot);
-                assertEquals(r['foo'], undefined);
-                assertEquals(r['bar'], undefined);
-                assertEquals(r['baz'], undefined);
+                equal(r['foo'], undefined);
+                equal(r['bar'], undefined);
+                equal(r['baz'], undefined);
             });
             shazbots.sort();
-            assertEquals(shazbots, [0, 1, 2]);
+            deepEqual(shazbots, [0, 1, 2]);
 
             widgets.update({ shazbot: 0 }, { shazbot: 420 });
 
             widgets.find({ shazbot: {"$lt": 1000}}).addCallback(function (results) {
                 cb++;
                 for (var i = 0; i < results.length; i++) {
-                    assertTrue(results[i].shazbot != 0);
+                    notEqual(results[i].shazbot, 0);
                 }
 
                 mongo.close();
@@ -111,5 +111,5 @@ mongo.addListener("connection", function () {
 mongo.connect({
     hostname: '127.0.0.1',
     port: 27017,
-    db: '__node_mongodb_test'
+    db: 'node_mongodb_test'
 });
